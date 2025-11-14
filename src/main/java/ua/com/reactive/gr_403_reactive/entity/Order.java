fc1bc440194cf03912606859949bc5cf33a68ba7
Дмитро <agentdmitro@gmail.com>
2025-11-14 23:46:23 +0200

    fixed orders java

--- src/main/java/ua/com/reactive/gr_403_reactive/entity/Order.java
+++ src/main/java/ua/com/reactive/gr_403_reactive/entity/Order.java
@@ -1,13 +1,25 @@
 package ua.com.reactive.gr_403_reactive.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table("orders")
 public class Order {
+
+    @Id
     private Long id;
+
+    @Column("drink_id")
     private Long drinkId;
-    private int sugarLevel;   // 0–3
-    private String status;    // NEW, MAKING, READY, ERROR
+
+    @Column("sugar_level")
+    private int sugarLevel;
+
+    @Column("status")
+    private String status;   // наприклад: "NEW", "DONE", "CANCELLED"
 }
